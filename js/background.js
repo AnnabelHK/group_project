86cbdad4e6d194e988634b98dce7579e9591ee30
AnnabelHK <56278527+AnnabelHK@users.noreply.github.com>
2019-11-24 18:50:13 +0000

    itinerary page
    added js file, itinerary page, added text instead of placeholders

---
+++ js/background.js
@@ -0,0 +1,14 @@
+
+
+  $('p.seven').hide();
+$('p.six').hide();
+$('p.five').hide();
+$('p.four').hide();
+$('p.three').hide();
+$('p.two').hide();
+$('p.one').hide();
+
+$('#ba').on('click',function () {
+  $('p.seven').show();
+})
+    // 1. log to the console that you have clicked the button
