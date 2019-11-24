d2333fc0c9f0f3e325a81aca04f565c785cb8985
AnnabelHK <56278527+AnnabelHK@users.noreply.github.com>
2019-11-24 23:37:41 +0000

    js and tidying
    added more javascript for itinerary pages, tidied things up, added viking page

--- js/background.js
+++ js/background.js
@@ -11,4 +11,31 @@ $('p.one').hide();
 $('#ba').on('click',function () {
   $('p.seven').show();
 })
-    // 1. log to the console that you have clicked the button
+
+$('#vi').on('click',function () {
+  $('p.three').show();
+})
+
+$('#sn').on('click',function () {
+  $('p.six').show();
+})
+
+$('#ci').on('click',function () {
+  $('p.one').show();
+})
+
+$('#ch').on('click',function () {
+  $('p.four').show();
+})
+
+$('#su').on('click',function () {
+  $('p.five').show();
+})
+
+$('#wa').on('click',function () {
+  $('p.two').show();
+})
+
+$('#print').on('click',function () {
+  window.print();
+})
